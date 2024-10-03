6da8c19bf11aa551e5a61351d1ae5165446a0278
Florian <mrminecrafttv19@gmail.com>
2024-10-04 00:45:49 +0200

    feat: meta / seo stuff

--- fdscluster-frontend/app/layout.tsx
+++ fdscluster-frontend/app/layout.tsx
@@ -1,9 +1,10 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import React from "react";
 import { ThemeProvider } from "next-themes";
 import { Open_Sans } from "next/font/google";
 import ThemeSwitcher from "@/components/fdscluster/theme-switcher";
+import Head from "@/components/fdscluster/head";
 
 const openSans = Open_Sans({
 	variable: "--font-open-sans",
@@ -12,8 +13,25 @@ const openSans = Open_Sans({
 });
 
 export const metadata: Metadata = {
-	title: "FDSCluster",
-	description: "The FedustriaCluster is a modern minecraft cloud solution."
+	title: "FedustriaCluster - Modern Minecraft Cloud Solution",
+	description: "The FedustriaCluster is a modern minecraft cloud solution.",
+	authors: [
+		{
+			name: "Fedox",
+			url: "https://fedox.ovh"
+		},
+		{
+			name: "Austria7",
+			url: "https://github.com/Austria7"
+		}
+	],
+	keywords: ["minecraft", "cloud", "cluster", "fdx", "fedustria", "fedustriacluster"]
+};
+
+export const viewport: Viewport = {
+	themeColor: "#2196F3",
+	initialScale: 1,
+	viewportFit: "cover"
 };
 
 export default function RootLayout({
@@ -23,6 +41,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
+			<Head />
 			<body className={`${openSans.variable} dark:bg-dark-one`}>
 				<ThemeProvider
 					attribute="class"
